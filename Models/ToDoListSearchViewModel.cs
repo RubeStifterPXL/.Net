9346f575cf9c29b29bd60389d86025a97b9b5ded
RubeStifterPXL <rube.stifter@student.pxl.be>
2026-01-20 18:39:13 +0100

    Add files via upload

---
+++ Models/ToDoListSearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ToDoListApp.Domain;
+
+namespace ToDoListApp.Web.Models;
+
+public class ToDoListSearchViewModel
+{
+    public string? TitleFilter { get; set; }
+    public IList<ToDoList> ToDoLists { get; set; }
+    public ToDoListSearchViewModel()
+    {
+        ToDoLists = new List<ToDoList>();
+        TitleFilter = null;
+    }
+}
