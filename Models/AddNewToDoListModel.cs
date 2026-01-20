94bf7dbbec5f2bff21930423ab70741124847f4e
RubeStifterPXL <rube.stifter@student.pxl.be>
2026-01-20 18:41:02 +0100

    Delete Models directory

--- Models/AddNewToDoListModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ToDoListApp.Web.Models;
-
-public class AddNewToDoListModel
-{
-    [Required, MinLength(4)]
-    public string Title { get; set; } = null!;
-}
--- Models/UpdateToDoItemModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ToDoListApp.Web.Models;
-
-public class UpdateToDoItemModel
-{
-    public bool IsDone { get; set; }
-}
