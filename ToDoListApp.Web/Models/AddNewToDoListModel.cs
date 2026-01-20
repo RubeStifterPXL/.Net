9470ce24a8326b694897f57ff748f10baa64a2da
RubeStifterPXL <rube.stifter@student.pxl.be>
2026-01-20 18:42:46 +0100

    Add files via upload

---
+++ ToDoListApp.Web/Models/AddNewToDoListModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListApp.Web.Models;
+
+public class AddNewToDoListModel
+{
+    [Required, MinLength(4)]
+    public string Title { get; set; } = null!;
+}
---
+++ ToDoListApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ToDoListApp.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
