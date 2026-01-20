fedd95f0873a3c7917987ba28a5f5f0742cb4d1c
RubeStifterPXL <rube.stifter@student.pxl.be>
2026-01-20 18:54:27 +0100

    Add files via upload

---
+++ ToDoListApp.Domain.Tests/ToDoListTests.cs
@@ -0,0 +1,14 @@
+namespace ToDoListApp.Domain.Tests;
+
+public class ToDoListTests
+{
+    public void CreateNew_ValidTitle_ShouldReturnNewListWithIdAndTitle()
+    {
+
+    }
+
+    public void CreateNew_TitleIsNullOrEmpty_ShouldThrowArgumentException()
+    {
+
+    }
+}
